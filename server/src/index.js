0fdd788970263b0ac4b1120991ea241d10ac6a5a
Alexandra Pana <pana.alexandra.06@gmail.com>
2026-01-09 14:48:10 +0200

    Backend setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,8 @@
 const app = require('./app');
+require('dotenv').config();
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
